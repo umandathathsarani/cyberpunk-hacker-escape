fb9e60e9471ab156096401b08cee2a19a1d71d7f
umandathathsarani <umathathsarni2003@gmail.com>
2026-05-27 17:12:47 +0530

    Added typewriter effect and localStorage auto-save functionality

--- js/game.js
+++ js/game.js
@@ -3,22 +3,53 @@ const choiceButtonsElement = document.getElementById('choice-buttons');
 const inventoryListElement = document.getElementById('inventory-list');
 
 let state = {};
+let typingInterval;
 
 function startGame() {
     state = {};
+    localStorage.removeItem('cyberpunkGameState');
+    localStorage.removeItem('cyberpunkCurrentNode');
     updateInventoryDisplay();
     showTextNode(1);
 }
 
-function showTextNode(textNodeIndex) {
-    const textNode = storyNodes.find(textNode => textNode.id === textNodeIndex);
+function loadGame() {
+    const savedState = localStorage.getItem('cyberpunkGameState');
+    const savedNode = localStorage.getItem('cyberpunkCurrentNode');
     
-    storyTextElement.innerText = textNode.text;
-    
-    while (choiceButtonsElement.firstChild) {
-        choiceButtonsElement.removeChild(choiceButtonsElement.firstChild);
+    if (savedState && savedNode) {
+        state = JSON.parse(savedState);
+        updateInventoryDisplay();
+        showTextNode(parseInt(savedNode));
+    } else {
+        startGame();
     }
+}
+
+function showTextNode(textNodeIndex) {
+    localStorage.setItem('cyberpunkCurrentNode', textNodeIndex);
+    localStorage.setItem('cyberpunkGameState', JSON.stringify(state));
+
+    const textNode = storyNodes.find(textNode => textNode.id === textNodeIndex);
+
+    clearInterval(typingInterval);
+    storyTextElement.innerText = '';
+    choiceButtonsElement.innerHTML = '';
     
+    let charIndex = 0;
+
+    typingInterval = setInterval(() => {
+        if (charIndex < textNode.text.length) {
+            storyTextElement.innerText += textNode.text.charAt(charIndex);
+            charIndex++;
+        } else {
+            clearInterval(typingInterval);
+            renderButtons(textNode);
+        }
+    }, 20);
+}
+
+function renderButtons(textNode) {
     textNode.options.forEach(option => {
         if (showOption(option)) {
             const button = document.createElement('button');
@@ -36,11 +67,11 @@ function showOption(option) {
 
 function selectOption(option) {
     const nextTextNodeId = option.nextText;
-    
+
     if (nextTextNodeId <= 0) {
         return startGame();
     }
-    
+
     state = Object.assign(state, option.setState);
     updateInventoryDisplay();
     showTextNode(nextTextNodeId);
@@ -48,9 +79,9 @@ function selectOption(option) {
 
 function updateInventoryDisplay() {
     inventoryListElement.innerHTML = '';
-    
+
     const items = Object.keys(state).filter(key => state[key] === true);
-    
+
     if (items.length === 0) {
         const li = document.createElement('li');
         li.innerText = 'EMPTY';
@@ -64,4 +95,4 @@ function updateInventoryDisplay() {
     }
 }
 
-startGame();
+loadGame();
